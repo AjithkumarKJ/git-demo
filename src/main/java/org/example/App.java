1e527c05077851a71ea667e0d737d36bcdd108c2
AjithkumarKJ <54181594+AjithkumarKJ@users.noreply.github.com>
2025-07-20 16:51:44 +0530

    Merged from develop (#2)
    * Init Commit 2
    
    * Commited from git bash
    
    * New JiraID-001 created (#1)

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,6 +8,9 @@ public class App
 {
     public static void main( String[] args )
     {
+        GitTest gt  = new GitTest();
+        gt.testDevelopBranch();
         System.out.println( "Hello Asha!" );
+
     }
 }
---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class GitTest {
+
+    void testDevelopBranch()
+    {
+
+        System.out.println("I am in develop branch");
+    }
+    void test1()
+    {
+        System.out.println("Checked merge from GitBash");
+    }
+    void test2()
+    {
+        System.out.println("To check branch rules");
+    }
+}
